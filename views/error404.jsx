967e9e78e26afd84e57c07c42621707ca7aad8ac
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-02-28 01:26:27 -0600

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,9 +7,17 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img src="/images/thought-catalog-sad-plate.jpg" alt="Empty Plate"/>
+                <div>Photo by <a href="https://unsplash.com/@thoughtcatalog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Thought Catalog</a> on <a href="https://unsplash.com/s/photos/empty-plate?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>
           </main>
       </Def>
     )
   }
 
 module.exports = error404
+
+
+  
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,11 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/sara-dubler-dinner.jpg" alt="Dinner Image"/>
+                <div>Photo by <a href="https://unsplash.com/@ahungryblonde_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sara Dubler</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
