f51ad56e41eefb0930b32d65c73d207cf4e86842
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-02-28 17:45:10 -0600

    Adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,8 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST-Rant</h1>
+              <a href='/places'>Check out these places!</a>
               <div>
                 <img src="/images/sara-dubler-dinner.jpg" alt="Dinner Image"/>
                 <div>Photo by <a href="https://unsplash.com/@ahungryblonde_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sara Dubler</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
