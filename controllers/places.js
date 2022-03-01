e3fdc625f56e6d891bf80beab3a8e0e6f85b9b9d
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-02-28 18:12:29 -0600

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,28 @@
 const router = require('express').Router()
-const places = require("../models/places")
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    res.render('places/index', { places })
+    res.send('places/index', { places })
+})
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 module.exports = router
