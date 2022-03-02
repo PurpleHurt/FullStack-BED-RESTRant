a300b79bf3781f791990ba0d4ab026c777f19e16
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-03-02 07:36:47 -0600

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -38,6 +38,17 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        res.render('places/edit', { place: places[id], id: id })
+    }
+})
+
 router.delete('/places/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
---
+++ views/places/edit.jsx
@@ -0,0 +1,46 @@
+const React = require('react')
+const Def = require('../default.jsx')
+
+function edit_form ({place, id}) {
+    return (
+        <Def>
+          <main>
+            <h1>Edit Place</h1>
+            <form method="POST" action={`/places/${id}?_method=PUT`}>
+            <div className="row">
+                <div className="form-group col-sm-6">
+                    <label htmlFor="name">Place Name</label>
+                    <input type="text" name="name" id="name"
+                    required defaultValue={place.name}  />
+            </div>
+            
+            <div className="form-group col-sm-6">
+                <label htmlFor="pic">Place Picture</label>
+                <input id="pic" type="text" name="pic" id="pic"
+                required defaultValue={place.city} />
+            </div>
+            </div>
+            <div className="row">
+                <div className="form-group col-sm-6">
+                    <label htmlFor="state">Place State</label>
+                    <input type="text" name="state" id="state"
+                    required defaultValue={place.state}  />
+            </div>
+            
+            <div className="form-group col-sm-6">
+                <label htmlFor="city">Place City</label>
+                <input id="city" type="text" name="city" id="city"
+                required defaultValue={place.city} />
+            </div>
+            </div>
+            <div className="form-group">
+                <label htmlFor="cuisines">Cuisines</label>
+                <input className="form-control" id="cuisines" name="cuisines" required />
+              </div>
+            </form>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = edit_form
