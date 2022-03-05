d496a28d4149e53ee418a75137d414c8928a6f2b
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-03-05 14:09:01 -0600

    Added connection to MongoDB

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,9 +5,10 @@ function show ({place, id}) {
     
     return (
         <Def>            
-          <main>          
-            <h1>{ place.name }</h1>
-            <a href={`/places/${id}/edit`} className="btn btn-warning">Edit</a>  
+          <main>
+          <h1>{ place.name }</h1>
+            <a href={`/places/${id}/edit`} className="btn btn-warning">Edit</a>          
+                          
             <form method="POST" action={`/places/${id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">Delete</button>
             </form> 
