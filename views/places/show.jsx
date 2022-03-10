bfd6a6be289ac84f5a6956d4f877ce8bfad5c6f3
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-03-09 18:13:46 -0600

    Added seeder file

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,18 +4,34 @@ const Def = require('../default')
 function show ({place, id}) {
     
     return (
-        <Def>            
-          <main>
+      <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
           <h1>{ place.name }</h1>
-            <a href={`/places/${id}/edit`} className="btn btn-warning">Edit</a>          
-                          
+            
+            <h2>Description</h2>
+          <h3>{data.place.showEstablished()}</h3>
+          <h4>Serving {data.place.cuisines}</h4>
+          <a href={`/places/${id}/edit`} className="btn btn-warning">Edit</a>                          
             <form method="POST" action={`/places/${id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">Delete</button>
             </form> 
-          </main>
-        </Def>
+          </div>
+    </div>
+  </main>
+</Def>
     )
-}
+    }
+
+
+
 
    
 
